87ac0d5bd3b81a609ad2bda257bda2aa1c453496
Bia <bia8717@hotmail.com>
2022-08-28 19:36:34 -0300

    ativando o cors

--- back-end/app/Http/Kernel.php
+++ back-end/app/Http/Kernel.php
@@ -14,7 +14,9 @@ class Kernel extends HttpKernel
      * @var array<int, class-string|string>
      */
     protected $middleware = [
-        // \App\Http\Middleware\TrustHosts::class,
+         \App\Http\Middleware\TrustHosts::class,
+        \Fruitcake\Cors\HandleCors::class,
+        \App\Http\Middleware\Cors::class,
         \App\Http\Middleware\TrustProxies::class,
         \Illuminate\Http\Middleware\HandleCors::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
